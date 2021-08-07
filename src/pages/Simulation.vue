5c2a830b8852d6d0389405096aa1da1f0644e091
Shahfiq Shah <shahfiq12@gmail.com>
2021-08-07 21:03:24 +0800

    powered commit

--- src/pages/Simulation.vue
+++ src/pages/Simulation.vue
@@ -1,31 +1,52 @@
 <template>
-    <q-page class="flex flex-center bg-primary q-pa-xl">
-        <q-card class="q-pa-md">
-            <q-card-section>
-                <div class="text-h6">Choose Material</div>
-            </q-card-section>
-            <q-card-section>
-                <q-select outlined dense label="Material" :options="options" v-model="select" />
-            </q-card-section>
-            <q-card-section>
-                <q-btn color="primary" to="/simulation/start">Start Simulation</q-btn>
-            </q-card-section>
-            <q-card-section>
-                <q-btn color="primary" to="/">HOME</q-btn>
-            </q-card-section>
-        </q-card>
-    </q-page>
+  <q-page class="flex flex-center bg-primary q-pa-xl">
+    <q-card class="q-pa-md">
+      <q-card-section>
+        <div class="text-h6">Choose Material</div>
+      </q-card-section>
+      <q-card-section>
+        <q-select
+          outlined
+          dense
+          label="Material"
+          :options="options"
+          v-model="select"
+        />
+      </q-card-section>
+      <q-card-section>
+        <q-btn color="primary" @click="alert">Start Simulation</q-btn>
+      </q-card-section>
+      <q-card-section>
+        <q-btn color="primary" to="/">HOME</q-btn>
+      </q-card-section>
+    </q-card>
+     <q-dialog v-model="dialog" position="bottom">
+    <q-card style="width: 350px">
+      <q-card-section class="row">
+        <q-btn color="primary" to="/simulation/start">Rocket Nozel</q-btn>
+        <q-space />
+        <q-btn color="primary">Iron Cassing</q-btn>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  </q-page>
+ 
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            options:[
-                'Please Choose'
-            ],
-            select:''
-        }
-    }
-}
+  data() {
+    return {
+      options: ["Please Choose"],
+      select: "",
+      dialog: false,
+    };
+  },
+  methods: {
+    alert() {
+        console.log("clicked")
+        this.dialog = !this.dialog;
+    },
+  },
+};
 </script>
